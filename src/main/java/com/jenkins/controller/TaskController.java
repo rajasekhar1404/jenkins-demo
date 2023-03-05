4ff485c8acafe3533cae978cdd75ca333f581e0f
rajasekhar1404 <mrrajasekhar09@gmail.com>
2023-03-05 14:30:41 +0530

    new end point added

--- src/main/java/com/jenkins/controller/TaskController.java
+++ src/main/java/com/jenkins/controller/TaskController.java
@@ -1,2 +1,21 @@
-package com.jenkins.controller;public class TaskController {
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TaskController {
+
+    @GetMapping("/print")
+    public String print() {
+        return "printing";
+    }
+
+    @GetMapping("/task")
+    public String getTask() {
+        return "Task created..";
+    }
+
 }
